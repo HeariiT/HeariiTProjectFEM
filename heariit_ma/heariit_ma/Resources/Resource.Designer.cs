d68166c2a98c1843aad0fad29450e20550fdf615
Unknown <juasmartinezbel@unal.edu.co>
2017-10-06 15:50:56 -0500

    Setting properties of Main and Item

--- heariit_ma/heariit_ma/Resources/Resource.Designer.cs
+++ heariit_ma/heariit_ma/Resources/Resource.Designer.cs
@@ -41,11 +41,70 @@ namespace heariit_ma
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int auriculares = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int linearLayout1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int listView1 = 2131034119;
+			
+			// aapt resource value: 0x7f050000
+			public const int relativeLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView2 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView5 = 2131034118;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int ItemRow = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Reproductive = 2130903042;
 			
 			static Layout()
 			{
@@ -60,8 +119,8 @@ namespace heariit_ma
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
