bd460f5e98afbd9ceb89ab617ba3ea7d44eee379
Unknown <juasmartinezbel@unal.edu.co>
2017-10-06 22:37:20 -0500

    Can now Pause, Play, advance and go back

--- heariit_ma/heariit_ma/Reproductive.cs
+++ heariit_ma/heariit_ma/Reproductive.cs
@@ -10,22 +10,38 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Media;
+using static Android.Views.View;
 
 namespace heariit_ma
 {
     [Activity(Label = "HeariiT Music")]
-    public class Reproductive : Activity{
+    public class Reproductive : Activity, MediaController.IMediaPlayerControl, IOnTouchListener {
 
         MediaPlayer _player;
         MediaController mediaController;
         LinearLayout linearLayout;
+
+        public int AudioSessionId{
+            get { return 0; }
+        }
+
+        public int BufferPercentage { get { return _player.CurrentPosition *100 /_player.Duration; } }
+
+        public int CurrentPosition {get { return _player.CurrentPosition; } }
+
+        public int Duration {get { return _player.Duration; } }
+
+        public bool IsPlaying {get { return _player.IsPlaying; } }
+
+       
+
 
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Reproductive);
             linearLayout = FindViewById<LinearLayout>(Resource.Id.linearLayout);
-            //linearLayout.SetOnTouchListener(this);
+            linearLayout.SetOnTouchListener(this);
             String urlAlbum = this.Intent.GetStringExtra("urlAlbum");
             String urlAudio = this.Intent.GetStringExtra("urlAudio");
             _player = new MediaPlayer();
@@ -34,8 +50,55 @@ namespace heariit_ma
         }
 
         private void playAudio(String urlAudio){
+            mediaController.SetMediaPlayer(this);
+            mediaController.SetAnchorView(FindViewById(Resource.Id.linearLayout));
+
             _player.SetDataSource(urlAudio);
             _player.Prepare();
+            _player.Start();
+        }
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            switch (e.Action) {
+                case MotionEventActions.Down:
+                    mediaController.Show();
+                    break;
+              
+            }
+
+            return true;
+        }
+
+        public bool CanPause()
+        {
+            return true;
+        }
+
+        public bool CanSeekBackward()
+        {
+            return true;
+        }
+
+        public bool CanSeekForward()
+        {
+            return true;
+        }
+
+
+
+        public void Pause()
+        {
+            _player.Pause();
+        }
+
+        public void SeekTo(int pos)
+        {
+            _player.SeekTo(pos);
+        }
+
+        public void Start()
+        {
             _player.Start();
         }
     }
